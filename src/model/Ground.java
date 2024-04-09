a7c133af35fe486bfb7ccb73be8ca1e89d2a8020
Eldar Zeynalli <eldar.zeynalli0101@gmail.com>
2024-04-09 20:44:18 +0200

    Ground.java created. for implementing the paths with cubes

---
+++ src/model/Ground.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.awt.Image;
+
+class Ground extends ActiveObject{
+    public Ground(int x, int y, int width, int height, Image image) {
+        super(x, y, width, height, image);
+        
+    }
+}
